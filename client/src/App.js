07283835226ca1bb52e51dc38fdb98d9764f602f
nenad78 <nenad.milutinovicdev@gmail.com>
2019-05-24 12:42:37 +0200

    alert action/reducer added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-
+import React, { Fragment, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import './App.css';
+
+import store from './store'
+import Alert from './components/layout/Alert'
 
 const App = () => {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <section className="container">
+            <Alert />
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  )
 }
 
 export default App;
