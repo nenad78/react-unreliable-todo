de63d76341ca191ab89904d48718e94290954ab8
nenad78 <nenad.milutinovicdev@gmail.com>
2019-05-24 13:20:38 +0200

    todo actions, reducer added

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import alert from './alert'
 import session from './session'
+import todo from './todo'
 
 export default combineReducers({
   alert,
-  session
+  session,
+  todo
 })
